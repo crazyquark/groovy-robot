527fabf0727cf3bf7055a06f54228c767c32672a
crazyquark <cristian.sandu@gmail.com>
2016-06-26 15:45:04 +0000

    rotate

--- motors.py
+++ motors.py
@@ -48,14 +48,14 @@ try:
 	#FORWARD
 	print("Forward")
 	m.MotorSpeedSetAB(100,100)	#defines the speed of motor 1 and motor 2;
-	m.MotorDirectionSet(0b1010)	#"0b1010" defines the output polarity, "10" means the M+ is "positive" while the M- is "negtive"
-	time.sleep(2)
+	m.MotorDirectionSet(0b1001)	#"0b1010" defines the output polarity, "10" means the M+ is "positive" while the M- is "negative"
+	time.sleep(0.1)
 
 	#BACK
-	print("Back")
-	m.MotorSpeedSetAB(100,100)
-	m.MotorDirectionSet(0b0101)	#0b0101  Rotating in the opposite direction
-	time.sleep(2)
+	#print("Back")
+	#m.MotorSpeedSetAB(100,100)
+	#m.MotorDirectionSet(0b0101)	#0b0101  Rotating in the opposite direction
+	#time.sleep(2)
 
 	#STOP
 	print("Stop")
@@ -63,13 +63,13 @@ try:
 	time.sleep(1)
 
 	#Increase speed
-	for i in range (100):
-		print("Speed:",i)
-		m.MotorSpeedSetAB(i,i)
-		time.sleep(.02)
+	#for i in range (100):
+	#	print("Speed:",i)
+	#	m.MotorSpeedSetAB(i,i)
+	#	time.sleep(.02)
 		
-	print("Stop")
-	m.MotorSpeedSetAB(0,0)	
+	#print("Stop")
+	#m.MotorSpeedSetAB(0,0)	
 	
 except IOError as e:
 	print("Unable to find the motor driver, check the addrees and press reset on the motor driver and try again")
