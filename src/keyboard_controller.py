70ff8bb9d8728dc3d63d1d5585040cd1ea760402
Cristian Sandu <cristian.sandu@gmail.com>
2016-10-16 09:50:51 +0300

    remove keyboard

--- src/keyboard_controller.py
+++ src/keyboard_controller.py
@@ -9,17 +9,12 @@ class KeyboardController:
 		self.robot = robot
 
 		# Press PAGE UP then PAGE DOWN to type "foobar".
-		#self.wHook = keyboard.add_hotkey('w', lambda: robot.move(Directions.Forward))
+		self.wHook = keyboard.add_hotkey('w', lambda: robot.move(Directions.Forward))
 		#self.sHook = keyboard.add_hotkey('s', lambda: robot.move(Directions.Back))
 		#self.aHook = keyboard.add_hotkey('a', lambda: robot.move(Directions.Left))
 		#self.dHook = keyboard.add_hotkey('d', lambda: robot.move(Directions.Right))
-		def handler(event):
-			print('Got: ' + event)
-
-		keyboard.listener.hotkeys[keyboard.os_keyboard.map_char(normalize_name('w'))] = handler
-		keyboard.listener.add_handler(handler)
 	def halt(self):
-		#keyboard.remove_hotkey('w')
+		keyboard.remove_hotkey('w')
 		#keyboard.remove_hotkey('s')
 		#keyboard.remove_hotkey('a')
 		#keyboard.remove_hotkey('s')
