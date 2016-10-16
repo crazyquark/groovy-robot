239583f11100ebb6bc3da6d14752c06601cc3206
Cristian Sandu <cristian.sandu@gmail.com>
2016-10-16 10:56:47 +0300

    more exc handling

--- src/run_server.py
+++ src/run_server.py
@@ -4,14 +4,16 @@
 '''
 
 from r_server import bottle_server
-import sys
+import sys, traceback
 
 enableKeyboard = True
 try:
 	from keyboard_controller import KeyboardController
 except:
 	print 'Keyboard error: probably there is no keyboard connected, disabling'
-	print sys.exc_info()[0]
+	print 'Cause might be: '
+	traceback.print_exc()
+
 	enableKeyboard = False
 
 if enableKeyboard:
