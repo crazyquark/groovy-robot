8d06d9393529b613513f5adde2b60dc42c694991
Cristian Sandu <cristian.sandu@gmail.com>
2016-10-16 13:12:40 +0300

    hook arrow keys

--- src/keyboard_controller.py
+++ src/keyboard_controller.py
@@ -6,15 +6,28 @@ from keyboard import keyboard
 
 from r_server.robot_server import Directions, Throttle
 
+ARROW_UP = 72
+ARROW_DOWN = 80
+ARROW_LEFT = 75
+ARROW_RIGHT = 77
+
 class KeyboardController:
 	'''Implements direct control using a connected USB keyboard'''
 	def __init__(self, robot):
 		# Press PAGE UP then PAGE DOWN to type "foobar".
 		self.hooks = []
+		#TODO: use a single handler for all keys
 		self.hooks.append(keyboard.hook_key('w', lambda: robot.move(Directions.Forward), lambda: robot.stop(Directions.Forward)))
+		self.hooks.append(keyboard.hook_key(ARROW_UP, lambda: robot.move(Directions.Forward), lambda: robot.stop(Directions.Forward)))
+
 		self.hooks.append(keyboard.hook_key('s', lambda: robot.move(Directions.Back), lambda: robot.stop(Directions.Back)))
+		self.hooks.append(keyboard.hook_key(ARROW_DOWN, lambda: robot.move(Directions.Back), lambda: robot.stop(Directions.Back)))
+
 		self.hooks.append(keyboard.hook_key('a', lambda: robot.move(Directions.Left), lambda: robot.stop(Directions.Left)))
+		self.hooks.append(keyboard.hook_key(ARROW_LEFT, lambda: robot.move(Directions.Left), lambda: robot.stop(Directions.Left)))
+
 		self.hooks.append(keyboard.hook_key('d', lambda: robot.move(Directions.Right), lambda: robot.stop(Directions.Right)))
+		self.hooks.append(keyboard.hook_key(ARROW_RIGHT, lambda: robot.move(Directions.Right), lambda: robot.stop(Directions.Right)))
 		
 		self.hooks.append(keyboard.hook_key('z', lambda: robot.speedAdjust(Throttle.Down), lambda: None))
 		self.hooks.append(keyboard.hook_key('x', lambda: robot.speedAdjust(Throttle.Up), lambda: None))
