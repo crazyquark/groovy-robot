d8bf2330be4228c904363010a80672cfbbf83619
Cristian Sandu <cristian.sandu@gmail.com>
2016-11-13 18:20:34 +0200

    1st attempt at fixing stream

--- src/r_server/bottle_server.py
+++ src/r_server/bottle_server.py
@@ -3,7 +3,8 @@
 	See http://bottlepy.org/docs/dev/async.html
 '''
 
-from bottle import request, Bottle, abort, template, static_file
+from bottle import request, Bottle, abort, template, static_file, Response
+from robot_server import Directions, Throttle
 
 app = Bottle()
 
@@ -57,9 +58,16 @@ from gevent.pywsgi import WSGIServer
 from geventwebsocket import WebSocketError
 from geventwebsocket.handler import WebSocketHandler
 
+def genStream(camera):
+    '''Video streaming generator function.'''
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+			   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/stream')
 def stream():
-	pass
+	return Response(genStream(camera))
 
 robot = None
 def run(robotServer, cameraServer):
