a94e008ed1490ac86df6f65a3a0b79ac9326626a
crazyquark <cristian.sandu@gmail.com>
2017-10-18 17:05:43 +0300

    cleanup

--- src/r_server/flask_server.py
+++ src/r_server/flask_server.py
@@ -1,6 +1,6 @@
 '''
-	Server implementation using flask
-	See http://bottlepy.org/docs/dev/async.html
+    Server implementation using flask
+    See http://bottlepy.org/docs/dev/async.html
 '''
 
 from flask import Flask, render_template as template, request, make_response, jsonify, Response
@@ -13,44 +13,47 @@ sockets = Sockets(app)
 
 @sockets.route('/ws')
 def websocket(wsock):
-	while not wsock.closed:
-		try:
-			message = wsock.receive()
-			if (message == 'hello'):
-				wsock.send('connected')
-			elif (message == 'w'):
-				robot.move(Directions.Forward)
-			elif (message == 'W'):
-				robot.stop(Directions.Forward)
-			elif (message == 's'):
-				robot.move(Directions.Back)
-			elif (message == 'S'):
-				robot.stop(Directions.Back)
-			elif (message == 'a'):
-				robot.move(Directions.Left)
-			elif (message == 'A'):
-				robot.stop(Directions.Left)
-			elif (message == 'd'):
-				robot.move(Directions.Right)
-			elif (message == 'D'):
-				robot.stop(Directions.Right)
-			elif (message == 'x'):
-				robot.speed_adjust(Throttle.Up)
-			elif (message == 'z'):
-				robot.speed_adjust(Throttle.Down)
-			elif (message == ''):
-				robot.stop()
-		except WebSocketError as err:
-			print repr(err)
-			break
+    '''
+        Server websocket
+    '''
+    while not wsock.closed:
+        try:
+            message = wsock.receive()
+            if message == 'hello':
+                wsock.send('connected')
+            elif message == 'w':
+                robot.move(Directions.Forward)
+            elif message == 'W':
+                robot.stop(Directions.Forward)
+            elif message == 's':
+                robot.move(Directions.Back)
+            elif message == 'S':
+                robot.stop(Directions.Back)
+            elif message == 'a':
+                robot.move(Directions.Left)
+            elif message == 'A':
+                robot.stop(Directions.Left)
+            elif message == 'd':
+                robot.move(Directions.Right)
+            elif message == 'D':
+                robot.stop(Directions.Right)
+            elif message == 'x':
+                robot.speed_adjust(Throttle.Up)
+            elif message == 'z':
+                robot.speed_adjust(Throttle.Down)
+            elif message == '':
+                robot.stop()
+        except WebSocketError as err:
+            print repr(err)
+            break
 
 @app.route('/')
 def index():
-	from urlparse import urlparse
-	parsedUrl = urlparse(request.url)
-	host = parsedUrl.hostname + (':' + str(parsedUrl.port) if parsedUrl.port else '')
-	
-	return template('main.html', host = host, resolution = (800, 600))
+    from urlparse import urlparse
+    parsedUrl = urlparse(request.url)
+    host = parsedUrl.hostname + (':' + str(parsedUrl.port) if parsedUrl.port else '')
+    
+    return template('main.html', host = host, resolution = (800, 600))
 
 @app.route('/images/<filename>')
 def images(filename):
@@ -61,28 +64,28 @@ from geventwebsocket import WebSocketError
 from geventwebsocket.handler import WebSocketHandler
 
 def genStream(camera):
-	'''Video streaming generator function.'''
-	frame = None
-	while True:
-		frame = camera.getFrame()
-		yield (b'--frame\r\n'
-			   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    '''Video streaming generator function.'''
+    frame = None
+    while True:
+        frame = camera.getFrame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/stream')
 def stream():
-	global camera
-	return Response(genStream(camera), mimetype='multipart/x-mixed-replace; boundary=frame') 
+    global camera
+    return Response(genStream(camera), mimetype='multipart/x-mixed-replace; boundary=frame') 
 
 robot = None
 def run(robotServer, cameraServer):
-	global robot, camera
-	robot = robotServer
-	camera = cameraServer
+    global robot, camera
+    robot = robotServer
+    camera = cameraServer
 
-	from gevent import pywsgi
-	from geventwebsocket.handler import WebSocketHandler
-	server = pywsgi.WSGIServer(('', 8080), app, handler_class=WebSocketHandler)
-	server.serve_forever()
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 8080), app, handler_class=WebSocketHandler)
+    server.serve_forever()
 
 def halt():
-	robot.halt()
+    robot.halt()
