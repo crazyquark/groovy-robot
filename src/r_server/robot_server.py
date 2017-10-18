82c6b58e061a1368d2448f4cfd498fc2cd962bc6
crazyquark <cristian.sandu@gmail.com>
2017-10-18 17:00:47 +0300

    code cleanup

--- src/r_server/robot_server.py
+++ src/r_server/robot_server.py
@@ -1,12 +1,23 @@
-import platform, sys, traceback, time
+'''
+    Robot Server
+'''
+import platform
+import traceback
 from threading import Thread
 
 from motors.adafruit_motors import AdafruitMotors
 
-class Directions:
+
+class Directions(object):
+    '''
+        Simple enumeration of the available directions
+    '''
     Forward, Back, Left, Right = range(4)
 
-class Throttle:
+class Throttle(object):
+    '''
+        Simple enumerations of the directions signs: up or down
+    '''
     Up, Down = (1, -1)
 
 class RobotServer(Thread):
@@ -15,53 +26,66 @@ class RobotServer(Thread):
         Uses an autostart thread to run its update loop
     '''
     def __init__(self):
-        self.fwdPressed = False
-        self.backPressed = False
-        self.leftPressed = False
-        self.rightPressed = False
+        self.fwd_pressed = False
+        self.back_pressed = False
+        self.left_pressed = False
+        self.right_pressed = False
 
-        self.turnFactor = 2 # controls how sharp the turns will be
+        self.turn_factor = 2 # controls how sharp the turns will be
 
         # How much to increase speed at one time
-        self.speedIncrement = 5
+        self.speed_increment = 5
 
         # Check if this is the real deal
         arch = platform.uname()[4]
-        self.runningOnPi = True if arch.startswith('arm') else False
+        self.running_on_pi = True if arch.startswith('arm') else False
 
         print('We are running on: ', arch)
 
         self.motors = AdafruitMotors()
-        
+
         Thread.__init__(self)
         self.running = True
         self.start()
-        
+
+    def no_key_pressed(self):
+        '''
+            Check the no keys pressed condition
+        '''
+        return not self.fwd_pressed and not self.back_pressed and \
+            not self.left_pressed and not self.right_pressed
+
+    def bad_key_combo(self):
+        '''
+            Check if both fowd and reverse are pressed or both left and right are pressed
+        '''
+        return (self.fwd_pressed and self.back_pressed) or \
+            (self.left_pressed and self.right_pressed)
+
     def run(self):
-        while(self.running):
-            try:	
-                if ((not self.fwdPressed and not self.backPressed and not self.leftPressed and not self.rightPressed) or
-                    (self.fwdPressed and self.backPressed) or (self.leftPressed and self.rightPressed)):
+        while self.running:
+            try:
+                if self.no_key_pressed() or self.bad_key_combo():
                     # Full stop
-                    self.motors.control_motors(0, 0)
-                elif (not self.leftPressed and not self.rightPressed and self.fwdPressed):
+                    self.motors.stop()
+                elif not self.left_pressed and not self.right_pressed and self.fwd_pressed:
                     # Full steam ahead!
                     self.motors.control_motors(100, 100)
-                elif (not self.leftPressed and not self.rightPressed and self.backPressed):
+                elif not self.left_pressed and not self.right_pressed and self.back_pressed:
                     # All engines reverse!
                     self.motors.control_motors(-100, -100)
-                elif (not self.backPressed and not self.fwdPressed and self.rightPressed):
+                elif not self.back_pressed and not self.fwd_pressed and self.right_pressed:
                     # In place right turn
                     self.motors.control_motors(-100, 100)
-                elif (not self.backPressed and not self.fwdPressed and self.leftPressed):
+                elif not self.back_pressed and not self.fwd_pressed and self.left_pressed:
                     # In place left turn
                     self.motors.control_motors(100, -100)
-                elif ((self.fwdPressed or self.backPressed) and self.rightPressed):
+                elif (self.fwd_pressed or self.back_pressed) and self.right_pressed:
                     # Attempt to turn right
-                    self.motors.control_motors(100 / self.turnFactor, 100)
-                elif ((self.fwdPressed or self.backPressed) and self.leftPressed):
+                    self.motors.control_motors(100 / self.turn_factor, 100)
+                elif (self.fwd_pressed or self.back_pressed) and self.left_pressed:
                     # Attempt to turn left
-                    self.motors.control_motors(100, 100 / self.turnFactor)
+                    self.motors.control_motors(100, 100 / self.turn_factor)
                 else:
                     # I donno
                     self.motors.control_motors(0, 0)
@@ -69,40 +93,55 @@ class RobotServer(Thread):
                 print 'Critical failure, shutting down'
                 print 'Possible cause: '
                 traceback.print_exc()
-                self.runnig = False
+                self.running = False
                 raise Exception('Motors failure')
 
-    def speedAdjust(self, direction):
-        if self.runningOnPi:
-            amount = direction * self.speedIncrement
+    def speed_adjust(self, direction):
+        '''
+            Increase or decrese speed depending on the sign of direction
+        '''
+        if self.running_on_pi:
+            amount = direction * self.speed_increment
             self.motors.change_speed(amount)
 
-    def processPress(self, direction, isOn):
+    def process_press(self, direction, is_on):
+        '''
+            Interpret a key press from UI
+        '''
         if direction == Directions.Forward:
-            self.fwdPressed = isOn
+            self.fwd_pressed = is_on
         elif direction == Directions.Back:
-            self.backPressed = isOn
+            self.back_pressed = is_on
         elif direction == Directions.Left:
-            self.leftPressed = isOn
+            self.left_pressed = is_on
         elif direction == Directions.Right:
-            self.rightPressed = isOn	
+            self.right_pressed = is_on
 
     def move(self, direction):
+        '''
+            Move robot
+        '''
         print('MOVE: ', direction)
 
-        if not self.runningOnPi:
+        if not self.running_on_pi:
             return
-        
-        self.processPress(direction, True)
+
+        self.process_press(direction, True)
 
     def stop(self, direction):
+        '''
+            Full stop
+        '''
         print('STOP: ', direction)
 
-        if not self.runningOnPi:
+        if not self.running_on_pi:
             return
 
-        self.processPress(direction, False)
+        self.process_press(direction, False)
 
     def halt(self):
+        '''
+            Stop main loop
+        '''
         self.running = False
         
