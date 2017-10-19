5f9c033c7add7eae0df092a1b45f575b2794db94
Cristian Sandu <cristian.sandu@gmail.com>
2017-10-19 20:58:08 +0300

    temp commit

--- src/r_server/static/client.js
+++ src/r_server/static/client.js
@@ -1,51 +1,53 @@
 'use strict'
 
-var ws = new WebSocket('ws://{{ host }}/ws');
-ws.onopen = function () {
-    ws.send('hello');
-};
-ws.onmessage = function (evt) {
-    console.log(evt.data);
-};
+function connect(host) {
+    var ws = new WebSocket('ws://' + host + '/ws');
+    ws.onopen = function () {
+        ws.send('hello');
+    };
+    ws.onmessage = function (evt) {
+        console.log(evt.data);
+    };
 
-var sendKey = (keyName) => {
-    if (keyName === 'w' || keyName === 'ArrowUp') {
-        ws.send('w');
-    }
-    else if (keyName === 's' || keyName === 'ArrowDown') {
-        ws.send('s');
-    }
-    else if (keyName === 'a' || keyName === 'ArrowLeft') {
-        ws.send('a');
-    }
-    else if (keyName === 'd' || keyName === 'ArrowRight') {
-        ws.send('d');
-    } else {
-        ws.send(keyName)
-    }
-};
+    var sendKey = (keyName) => {
+        if (keyName === 'w' || keyName === 'ArrowUp') {
+            ws.send('w');
+        }
+        else if (keyName === 's' || keyName === 'ArrowDown') {
+            ws.send('s');
+        }
+        else if (keyName === 'a' || keyName === 'ArrowLeft') {
+            ws.send('a');
+        }
+        else if (keyName === 'd' || keyName === 'ArrowRight') {
+            ws.send('d');
+        } else {
+            ws.send(keyName)
+        }
+    };
 
-var keydownHandler = (event) => {
-    // Ignore repeated events FFS
-    if (event.repeat) {
-        return
-    }
+    var keydownHandler = (event) => {
+        // Ignore repeated events FFS
+        if (event.repeat) {
+            return
+        }
 
-    const keyName = event.key.toLowerCase();
+        const keyName = event.key.toLowerCase();
 
-    console.log('down: ' + keyName)
+        console.log('down: ' + keyName)
 
-    sendKey(keyName)
-};
+        sendKey(keyName)
+    };
 
-var keyupHandler = (event) => {
-    const keyName = event.key.toUpperCase();
+    var keyupHandler = (event) => {
+        const keyName = event.key.toUpperCase();
 
-    console.log('up: ' + keyName)
+        console.log('up: ' + keyName)
 
-    sendKey(keyName)
+        sendKey(keyName)
 
-};
+    };
 
-document.addEventListener('keydown', keydownHandler, false);
-document.addEventListener('keyup', keyupHandler, false);
+    document.addEventListener('keydown', keydownHandler, false);
+    document.addEventListener('keyup', keyupHandler, false);
+}
