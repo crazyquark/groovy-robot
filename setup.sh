615639dc77399f4d95397562b808285c309e76de
Cristian Sandu <cristian.sandu@gmail.com>
2017-10-20 16:33:00 +0300

    yet another dependency

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ apt install python3-dev
 apt install python3-pip
 apt install python3-pygame
 apt install python3-opencv
+apt install python3-smbus
 
 pushd lib/adafruit_motor_hat
 python3 setup.py install
