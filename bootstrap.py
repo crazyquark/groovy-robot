fa56e274f94674bc07e88dc9b6c51ff0b983c432
Cristian Sandu <cristian.sandu@gmail.com>
2017-11-12 13:57:00 +0200

    attempt close

--- bootstrap.py
+++ bootstrap.py
@@ -13,13 +13,16 @@ device = None
 for dev in devices:
     if dev.name == 'Sony Computer Entertainment Wireless Controller':
         device = dev
+        print('Found controller')
         break
 
 if device:
+    print('Waiting for input')
     for event in device.read_loop():
         # Start pressed, yo!
         if event.type == 1 and event.code == 291 and event.value == 1:
             # Bootstrap our server
+            device.close()
             print('Start pressed, good job!')
             err = subprocess.check_call(['./run_server.sh'])
             print('Process result: ', err)
