ead0478efd3fb4cfb7f5799d903f09b9ae139b47
Cristian Sandu <cristian.sandu@gmail.com>
2017-11-18 22:24:31 +0200

    oops

--- src/r_server/display.py
+++ src/r_server/display.py
@@ -84,7 +84,7 @@ class PiDisplay(Thread):
             return
 
         while self.running:
-            Text has not changed
+            # Text has not changed
             if not self.refresh:
                 time.sleep(.1)
                 continue
