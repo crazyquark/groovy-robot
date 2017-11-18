39a88950f118c5170c7d2948eef285e349bd4d64
Cristian Sandu <cristian.sandu@gmail.com>
2017-11-18 18:27:08 +0200

    simplify

--- src/r_server/display.py
+++ src/r_server/display.py
@@ -81,21 +81,21 @@ class PiDisplay(Thread):
             self.draw.rectangle((0, 0, self.width, self.height), outline = 0, fill = 0)
 
             # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-            cmd = "hostname -I | cut -d\' \' -f1"
-            IP = subprocess.check_output(cmd, shell = True )
-            cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-            CPU = subprocess.check_output(cmd, shell = True )
-            cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-            MemUsage = subprocess.check_output(cmd, shell = True )
-            cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-            Disk = subprocess.check_output(cmd, shell = True )
+            # cmd = "hostname -I | cut -d\' \' -f1"
+            # IP = subprocess.check_output(cmd, shell = True )
+            # cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+            # CPU = subprocess.check_output(cmd, shell = True )
+            # cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+            # MemUsage = subprocess.check_output(cmd, shell = True )
+            # cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+            # Disk = subprocess.check_output(cmd, shell = True )
 
             # Write two lines of text.
 
-            self.draw.text((self.x, self.top),       "IP: " + str(IP, 'utf-8'),  font=self.font, fill=255)
-            self.draw.text((self.x, self.top+8),     str(CPU, 'utf-8'), font = self.font, fill = 255)
-            self.draw.text((self.x, self.top+16),    str(MemUsage, 'utf-8'),  font=self.font, fill=255)
-            self.draw.text((self.x, self.top+25),    str(Disk, 'utf-8'),  font=self.font, fill=255)
+            self.draw.text((self.x, self.top), 'Hi there',  font=self.font, fill=255)
+            # self.draw.text((self.x, self.top+8),     str(CPU, 'utf-8'), font = self.font, fill = 255)
+            # self.draw.text((self.x, self.top+16),    str(MemUsage, 'utf-8'),  font=self.font, fill=255)
+            # self.draw.text((self.x, self.top+25),    str(Disk, 'utf-8'),  font=self.font, fill=255)
 
             # Display image.
             self.disp.image(self.image)
