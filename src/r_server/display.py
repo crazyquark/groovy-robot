a89ca3b490d499d9efb9eab0512ae1e78c696b34
DESKTOP-HRFUFM2\kris_ <cristian.sandu@gmail.com>
2018-03-04 14:45:20 +0200

    hack display

--- src/r_server/display.py
+++ src/r_server/display.py
@@ -28,36 +28,36 @@ class PiDisplay(Thread):
 
         # rev.1 users set port=0
         # substitute spi(device=0, port=0) below if using that interface
-        serial = i2c(port=1, address=0x3C)
+        serial = i2c(port=1, address=0x70)
 
         # substitute ssd1331(...) or sh1106(...) below if using that device
         self.device = ssd1306(serial)
 
         # Create blank image for drawing.
         # Make sure to create image with mode '1' for 1-bit color.
-        self.width = self.disp.width
-        self.height = self.disp.height
-        self.image = Image.new('1', (self.width, self.height))
+        # self.width = self.disp.width
+        # self.height = self.disp.height
+        # self.image = Image.new('1', (self.width, self.height))
 
-        # Get drawing object to draw on image.
-        self.draw = ImageDraw.Draw(self.image)
+        # # Get drawing object to draw on image.
+        # self.draw = ImageDraw.Draw(self.image)
 
-        # Draw a black filled box to clear the image.
-        self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
+        # # Draw a black filled box to clear the image.
+        # self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
 
-        # Draw some shapes.
-        # First define some constants to allow easy resizing of shapes.
-        padding = -2
-        self.top = padding
-        self.bottom = self.height - padding
-        # Move left to right keeping track of the current x position for drawing shapes.
+        # # Draw some shapes.
+        # # First define some constants to allow easy resizing of shapes.
+        # padding = -2
+        # self.top = padding
+        # self.bottom = self.height - padding
+        # # Move left to right keeping track of the current x position for drawing shapes.
 
-        # Load default font.
-        self.font = ImageFont.load_default()
+        # # Load default font.
+        # self.font = ImageFont.load_default()
 
-        # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-        # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-        # font = ImageFont.truetype('Minecraftia.ttf', 8)
+        # # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+        # # Some other nice fonts to try: http://www.dafont.com/bitmap.php
+        # # font = ImageFont.truetype('Minecraftia.ttf', 8)
 
         self.refresh = True
         self.text = ['Hi there']
