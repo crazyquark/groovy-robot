7394006a981fd5757f8d6144269c2bc92851d332
crazyquark <cristian.sandu@gmail.com>
2018-09-01 13:50:32 +0300

    speeds

--- src/motors/stepper_motor.py
+++ src/motors/stepper_motor.py
@@ -14,15 +14,15 @@ class StepperMotor:
         if self.running_on_pi:
             self.motors = adafruit_motors.motors
 
-            self.stepper = self.motors.getStepper(100, 2) # 200 steps/rev, motor port #2
-            self.stepper.setSpeed(60)                     # 60 RPM
+            self.stepper = self.motors.getStepper(200, 2) # 200 steps/rev, motor port #2
+            self.stepper.setSpeed(30)                     # 30 RPM
     
     def rotate(self, steps):
         if not self.running_on_pi:
             return
 
         direction = Adafruit_MotorHAT.FORWARD if steps > 0 else Adafruit_MotorHAT.BACKWARD
-        self.stepper.step(abs(steps), direction, Adafruit_MotorHAT.INTERLEAVE)
+        self.stepper.step(abs(steps), direction, Adafruit_MotorHAT.SINGLE)
 
     def stop(self):
         if not self.running_on_pi:
