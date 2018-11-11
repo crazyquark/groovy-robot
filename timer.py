8987eefd91662b8e3a219a44fa9e70bdafcf6f5f
crazyquark <cristian.sandu@gmail.com>
2018-11-11 15:46:04 +0200

    flush

--- timer.py
+++ timer.py
@@ -1,8 +1,10 @@
 #!/bin/python3
 from datetime import datetime as dt
+import time
 
 with open('timer.txt','w') as ft:
     ft.write(dt.now().strftime('Started: %H:%M %d-%m-%y\n'))
     while True:
         ft.write(dt.now().strftime('\r%H:%M %d-%m-%y'))
-
+        ft.flush()
+        time.sleep(30)
