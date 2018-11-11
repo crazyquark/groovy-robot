c1c537903befa5ca13aad1fb432b22b10715a28b
crazyquark <cristian.sandu@gmail.com>
2018-11-11 15:24:35 +0200

    timer

--- timer.py
+++ timer.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 from datetime import datetime as dt
+
 with open('timer.txt','w') as ft:
-	while True:
+    ft.write(dt.now().strftime('Started: %H:%M %d-%m-%y'))
+    while True:
 		ft.write(dt.now().strftime('\r%H:%M %d-%m-%y'))
 
