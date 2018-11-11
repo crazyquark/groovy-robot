9f9b1cf5154e3444398180177aa2c0e99548a3bb
crazyquark <cristian.sandu@gmail.com>
2018-11-11 15:27:08 +0200

    spaces

--- timer.py
+++ timer.py
@@ -4,5 +4,5 @@ from datetime import datetime as dt
 with open('timer.txt','w') as ft:
     ft.write(dt.now().strftime('Started: %H:%M %d-%m-%y'))
     while True:
-		ft.write(dt.now().strftime('\r%H:%M %d-%m-%y'))
+        ft.write(dt.now().strftime('\r%H:%M %d-%m-%y'))
 
