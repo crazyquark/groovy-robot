e9ba8a7061ca8054d2e7fc80ffa0424a34b526c3
crazyquark <cristian.sandu@gmail.com>
2018-11-18 19:23:43 +0200

    try to use pixy camera

--- src/camera/pixy_camera.py
+++ src/camera/pixy_camera.py
@@ -3,9 +3,10 @@ from sys import path
 path.insert(0, '../lib/pixy2/build/python_demos')
 
 import pixy
-from pixy import Uint32Array
+from PIL import Image
 
 from .camera import Camera
+
 
 class PixyCamera(Camera):
     '''
@@ -18,9 +19,14 @@ class PixyCamera(Camera):
         pixy.init()
         pixy.change_prog('video')
 
+        self.frame_width = 316
+        self.frame_height = 208
+
     def get_frame(self):
-        self.frame = Uint32Array(316*208)
-        pixy.video_get_raw_frame(self.frame)
+        raw_frame = pixy.video_get_raw_frame(
+            self.frame_width*self.frame_height*3)
+        self.frame = Image.fromarray(raw_frame.reshape(
+            self.frame_height, self.frame_width, 3))
 
         self.compute_fps()
 
