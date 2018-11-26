e42ae80af4ef72e326a1a0521c476455020f8947
crazyquark <cristian.sandu@gmail.com>
2018-11-26 21:33:14 +0000

    output

--- src/debug/debuggable_process.py
+++ src/debug/debuggable_process.py
@@ -11,8 +11,8 @@ class DebuggableProcess(Process):
         super(DebuggableProcess, self).__init__()
 
     def enable_logging(self, name):
-        sys.stdout = open(name + '.out', 'a', buffering=0)
-        sys.stderr = open(name + '_error.out', 'a', buffering=0)
+        sys.stdout = open(name + '.out', 'a')
+        sys.stderr = open(name + '_error.out', 'a')
 
     def enable_debug(self, port=5678):
         enable_attach(address=('0.0.0.0', port), redirect_output=True)
