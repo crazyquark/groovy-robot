0dadc8a4a6da2b456aecbb5cf827552b3d745fc8
crazyquark <cristian.sandu@gmail.com>
2020-03-18 13:20:19 +0200

    init migration to odroid c2

--- setup-odroid.sh
+++ setup-odroid.sh
@@ -1,41 +1,38 @@
-echo 'apt'
-apt install -y git
-apt install -y i2c-tools
-apt install -y python3-pip
-apt install -y python3-pil
-apt install -y python3-numpy
-apt install -y python3-smbus
-apt install -y python3-evdev
-apt install -y python3-pyaudio
-apt install -y python3-cffi
-apt install -y python3-opencv
-apt install -y libusb-dev
-apt install -y joystick
-apt install -y libbluetooth-dev
-apt install -y bluez
-apt install -y pkg-config
-apt install -y checkinstall
+#!/bin/bash
+# Adapted for Armbian Buster
+echo 'apt install'
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y avahi-daemon
 
-echo 'wiringPi'
-git clone https://github.com/hardkernel/wiringPi
-pushd wiringPi
-./build
-popd
+sudo apt install -y git
+sudo apt install -y i2c-tools
+sudo apt install -y python3
+sudo apt install -y python3-pip
+sudo apt install -y python3-pil
+sudo apt install -y python3-numpy
+sudo apt install -y python3-smbus
+sudo apt install -y python3-evdev
+sudo apt install -y python3-pyaudio
+sudo apt install -y python3-cffi
+# sudo apt install -y python3-opencv
+sudo apt install -y libusb-dev
+sudo apt install -y joystick
+# sudo apt install -y libbluetooth-dev
+# sudo apt install -y bluez
+sudo apt install -y pkg-config
+sudo apt install -y checkinstall
 
-git clone --recursive https://github.com/hardkernel/WiringPi2-Python
-pushd WiringPi2-Python
-python3 setup.py install
-python setup.py install
-popd
+sudo adduser $USER i2c
+
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/libgpiod.sh
+chmod +x libgpiod.sh
+./libgpiod.sh
 
 git clone https://github.com/jfath/RPi.GPIO-Odroid.git
 pushd RPi.GPIO-Odroid
 python3 setup.py build install
 python setup.py build install
-popd
-
-pushd lib/adafruit_motor_hat
-python3 setup.py install
 popd
 
 echo 'sixad'
@@ -47,11 +44,7 @@ checkinstall
 dpkg -i sixad_*.deb
 popd
 
-echo 'modules'
-echo aml_i2c >> /etc/modules
-echo spicc >> /etc/modules
-echo spidev >> /etc/modules
-
 echo 'pip3'
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
