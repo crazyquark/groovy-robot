040153d157ab77d741345011915751a9f528952e
crazyquark <cristian.sandu@gmail.com>
2020-03-18 15:26:51 +0200

    fix GPIO permissions

--- setup-odroid.sh
+++ setup-odroid.sh
@@ -15,36 +15,39 @@ sudo apt install -y python3-smbus
 sudo apt install -y python3-evdev
 sudo apt install -y python3-pyaudio
 sudo apt install -y python3-cffi
-# sudo apt install -y python3-opencv
 sudo apt install -y libusb-dev
 sudo apt install -y joystick
-# sudo apt install -y libbluetooth-dev
-# sudo apt install -y bluez
+sudo apt install -y libbluetooth-dev 
 sudo apt install -y pkg-config
 sudo apt install -y checkinstall
+sudo apt install -y linux-headers-current-meson64
+sudo apt install -y python3-libgpiod
+# maybe:
+# sudo apt install -y python3-opencv
+# sudo apt install -y bluez
 
 sudo adduser $USER i2c
 
-wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/libgpiod.sh
-chmod +x libgpiod.sh
-./libgpiod.sh
-
 git clone https://github.com/jfath/RPi.GPIO-Odroid.git
 pushd RPi.GPIO-Odroid
-python3 setup.py build install
-python setup.py build install
+sudo python3 setup.py build install
 popd
+rm -rf RPi.GPIO-Odroid
 
 echo 'sixad'
 git clone https://github.com/RetroPie/sixad.git
 pushd sixad
 make
 mkdir -p /var/lib/sixad/profiles
-checkinstall
-dpkg -i sixad_*.deb
+sudo checkinstall
 popd
+rm -rf sixad
 
 echo 'pip3'
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
+
+echo 'gpio'
+sudo cp ./config/97-gpio.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules && sudo udevadm trigger
 
