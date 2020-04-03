dabf7462191a4a6276afd14523ad3a989f0f574c
crazyquark <cristian.sandu@gmail.com>
2020-04-03 21:43:54 +0300

    try to supress alsa errors

--- src/microphone/mic_capture.py
+++ src/microphone/mic_capture.py
@@ -1,21 +1,22 @@
-from io import BytesIO
-
 import queue
 import pyaudio
+from ctypes import *
 
-FORMAT = pyaudio.paFloat32
+FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 4096
-MAX_FRAMES = 5
+MAX_FRAMES = 20
 
 class MicCapture:
     ''' Captures mic input as audio chunks '''
 
     def __init__(self):
-        self.queue = queue.Queue()
+        self.queue = queue.Queue(maxsize=MAX_FRAMES)
 
+        self.alsaMessageSuppress()
         self.audio = pyaudio.PyAudio()
+        
         self.stream = self.audio.open(format=FORMAT,
             rate=RATE, input=True, 
             channels=CHANNELS,
@@ -25,12 +26,21 @@ class MicCapture:
 
     def stream_callback(self, in_data, frame_count, time_info, status_flags):
         ''' Get an audio chunk from internal stream '''
-        data = in_data.copy()
-        self.queue.put(data)
-        return (data, pyaudio.paContinue)
+        self.queue.put(in_data)
+        return (in_data, pyaudio.paContinue)
 
     def close(self):
         ''' Close stream '''
         self.stream.stop_stream()
         self.stream.close()
         self.audio.terminate()
+
+    def alsaMessageSuppress(self):
+        ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+        def py_error_handler(filename, line, function, err, fmt):
+            print('Debug Message Suppressed')
+        
+        c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+        asound = cdll.LoadLibrary('libasound.so.2')
+        # Set error handler
+        asound.snd_lib_error_set_handler(c_error_handler)
