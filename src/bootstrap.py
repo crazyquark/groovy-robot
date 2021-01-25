f540e7b928780b435dce265b8330ee5255f609b4
crazyquark <cristian.sandu@gmail.com>
2021-01-25 13:34:52 +0200

    pair ps3 controller

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -6,11 +6,11 @@ import evdev
 import sys
 import subprocess
 
-from r_server import display
-from r_server.ps3_controller import SixAxisButtonCodes
+# from r_server import display
+from controllers.ps3_controller import SixAxisButtonCodes
 
-display = display.PiDisplay()
-display.set_text(['Init'])
+# display = display.PiDisplay()
+# display.set_text(['Init'])
 
 device = None
 while not device:
@@ -20,7 +20,7 @@ while not device:
         if dev.name == 'Sony Computer Entertainment Wireless Controller':
             device = dev
             print('Found controller')
-            display.append_text('Controller ON')
+            # display.append_text('Controller ON')
             break
 
 if device:
@@ -28,12 +28,12 @@ if device:
     for event in device.read_loop():
 	    # Start pressed, yo!
         if event.type == 1 and event.code == SixAxisButtonCodes.Start and event.value == 1:
-            display.append_text('Starting server')
+            # display.append_text('Starting server')
             # Bootstrap our server
             break
 
     del device
-    display.halt()
+    # display.halt()
 
     print('Start pressed, good job!')
     err = subprocess.check_call(['/home/pi/groovy-robot/start_server.sh'])
